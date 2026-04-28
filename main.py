46ade1e51ae46cde46ae92d96c85e754e38911ca
ddnetwork-ent <ddbusiness.co@outlook.com>
2026-04-28 12:05:07 +0800

    fix render startup

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import Response
-from rembg import remove
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.post("/remove-bg")
 async def remove_bg(file: UploadFile = File(...)):
+    from rembg import remove
+
     input_data = await file.read()
     output_data = remove(input_data)
 
